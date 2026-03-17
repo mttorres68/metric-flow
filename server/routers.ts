793bc32b9e7390b5426650e5e5c4cf44f85cd5cd
Mateus Torres <mateusmmt00@gmail.com>
2026-03-17 08:10:19 -0300

    Add compliance router to appRouter

--- server/routers.ts
+++ server/routers.ts
@@ -4,12 +4,15 @@ import { systemRouter } from "./_core/systemRouter";
 import { publicProcedure, router } from "./_core/trpc";
 import { dashboardRouter } from "./routers/dashboardRouter";
 import { vendedoresRouter } from "./routers/vendedoresRouter";
+import { complianceRouter } from "./routers/complianceRouter ";
+
 
 export const appRouter = router({
     // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
   system: systemRouter,
   dashboard: dashboardRouter,
   vendedores: vendedoresRouter,
+  compliance: complianceRouter,
   auth: router({
     me: publicProcedure.query(opts => opts.ctx.user),
     logout: publicProcedure.mutation(({ ctx }) => {
